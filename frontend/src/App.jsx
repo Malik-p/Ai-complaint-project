e43440f1addbf59ca47140133270f7ecccc856c4
Malik-p <sahilmaik2005@gmail.com>
2025-12-23 11:29:50 +0530

    basic layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center text-5xl font-bold text-emerald-600">
+      Tailwind Fixed ✅
+    </div>
+  );
+}
